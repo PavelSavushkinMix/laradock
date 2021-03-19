ab898bf3ef9c3cff1f9c8ee41a0ce7177e831000
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2021-03-20 01:23:00 +0200

    fix(dev tool): avoid creating laradock user for workspace

--- testing/common/workspace.sh
+++ testing/common/workspace.sh
@@ -1,9 +1,6 @@
 set -xe;
 apt-get update -yqq && \
 pecl channel-update pecl.php.net && \
-groupadd -g ${PGID} laradock && \
-useradd -l -u ${PUID} -g laradock -m laradock -G docker_env && \
-usermod -p "*" laradock -s /bin/bash && \
 apt-get install -yqq \
   apt-utils \
   libzip-dev \
